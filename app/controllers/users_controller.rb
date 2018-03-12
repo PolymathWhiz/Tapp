e12ae07b7bb5c18fa81437031a08d0c091c4d1f5
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-03-12 23:41:24 +0000

    edging closer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show]
   layout 'application'
-  
-  before_action :set_employer
-  
-  def show
-  end
+
+  def show; end
 
   private
-  def set_employer
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  devise_for :users
+   devise_for :users, controllers: {
+    sessions: 'user/sessions',
+    registrations: 'user/registrations'
+  }
   
   devise_scope :users do
     resources :users, only: [:show]
   end
-  # devise_for :users, controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations'
-  # }
+
+ 
 end
