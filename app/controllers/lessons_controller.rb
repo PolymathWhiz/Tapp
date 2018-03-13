7e8edcfb2449af3e6ef0d40412e6bfe316e694e1
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-03-13 01:27:01 +0000

    working with lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :set_lession, only: [:update, :destroy]
+  
+  def index
+   @lessons = Lessons.all 
+  end
+
+  def create
+    # @lesson = Lesson.new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def set_lesson
+      @lesson =  Lesson.find(params[:id])
+    end
 end
