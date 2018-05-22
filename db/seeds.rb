d763a1678549cf6d8cc29483c7e243edb936787e
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-05-22 09:22:55 +0000

    done with seeds... final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 admin = User.create({ fn: 'John', lname: 'Doe', email: 'john@doe.com', password: 'password', admin: true })
 user = User.create({ fn: 'John', lname: 'Wick', email: 'john@wick.com', password: 'password', admin: false })
+tari = User.create({ fn: 'Tari', lname: 'Egule', email: 't.i4me@yahoo.com', password: 'password', admin: true })
+tombara = User.create({ fn: 'Tombara', lname: 'Egule', email: 'tombara@gmail.com', password: 'password', admin: false })
 admin.save!
 user.save!
+tari.save!
+tombara.save!
